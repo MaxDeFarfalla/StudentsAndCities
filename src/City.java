2c69897d3967776bcb3284961d608678ac1fbfba
MaxDeFarfalla <Maximilian.defarfalla@elev.ga.ntig.se>
2023-10-05 11:26:38 +0200

    Started

--- src/City.java
+++ src/City.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class City {
+public class City {
+//Attr
+    private String country;
+    private String name;
+
+    //Constr
+
+    public City(String country, String name){
+        this.country = country;
+        this.name = name;
+    }
+
+    //Getters
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+//Attr
+    private Student buddy;
+    private City hometown;
+    private String name;
+    private int studentId;
+
+    //Constr
+    public String reportBuddyName(){
+        return;
+    }
+
 }
